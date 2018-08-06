8a23c471bceadddcced348f987dcfa6077d4559a
kranzkrone <dev@st-page.de>
2018-08-06 18:24:14 +0200

    Tutorial 5

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -4,9 +4,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<pk>/', views.SnippetDetail.as_view()),    
-    path('users/', views.UserList.as_view()),
-    path('users/<pk>/', views.UserDetail.as_view()),
+    path('snippets/<pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),    
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('', views.api_root),
+    path('snippets/<pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
